329efc278fd92228182140338175754aa879a3b8
aharon <ahla85@gmail.com>
2024-01-16 03:11:44 +0200

    finalize deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.23 <0.9.0;
+pragma solidity ^0.8.23;
 
+import { Script } from "forge-std/src/Script.sol";
 import { StakeManager } from "../src/StakeManager.sol";
 
-import { BaseScript } from "./Base.s.sol";
+contract Deploy is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
-contract Deploy is BaseScript {
-    function run() public broadcast returns (StakeManager stakeManager) {
-        stakeManager = new StakeManager();
+        StakeManager stakeManager = new StakeManager();
+
+        vm.stopBroadcast();
     }
 }
