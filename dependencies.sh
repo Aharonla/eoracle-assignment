576451ba98619488eb2f436a2ac3973582972cf8
aharon <ahla85@gmail.com>
2024-01-17 19:16:57 +0200

    update config files

---
+++ dependencies.sh
@@ -0,0 +1,4 @@
+forge install OpenZeppelin/openzeppelin-contracts --no-commit
+forge install OpenZeppelin/openzeppelin-foundry-upgrades --no-commit
+forge install OpenZeppelin/openzeppelin-contracts-upgradeable --no-commit
+forge install foundry-rs/forge-std --no-commit
